684cbff23100fb84bfb5df195323c9fc8ac70fbe
Maksim Ivanov <vonavimax@gmail.com>
2025-01-24 23:05:27 +0700

    feat: mod options complete with fancy utils

--- Contents/mods/Standardized Reloading XP/42/media/lua/shared/ST_Reloading_ModOptions.lua
+++ Contents/mods/Standardized Reloading XP/42/media/lua/shared/ST_Reloading_ModOptions.lua
@@ -29,76 +29,46 @@ function modOptions.init()
 	}
 	modOptions.ComboBoxChance = ComboBoxChance
 
-	options:addDescription(getText("UI_options_STRELOAD_B42_chance_description"))
-
-	local ComboBoxReduction = options:addCheckBox(
-		"ST_Reloading_ComboBoxReduction",
-		getText("UI_options_STRELOAD_B42_reduction_label")
-	)
-	modOptions.ComboBoxReduction = ComboBoxReduction
-
-	MVXIVY_Utils.addComboBoxItems(
-		ComboBoxReduction, 
-		{ 
+	local ComboBoxReduction = ComboBoxFactory{
+		name="ComboBoxReduction",
+		label="reduction_label",
+		items={ 
 			getText("UI_options_STRELOAD_B42_reduction_item_yes"),
 			getText("UI_options_STRELOAD_B42_reduction_item_no") 
 		},
-		2
-	)
+		defaultItem=1
+		description="reduction_description"
+	}
+	modOptions.ComboBoxReduction = ComboBoxReduction
 
-	options:addDescription(getText("UI_options_STRELOAD_B42_reduction_description"))
+	local	ComboBoxRewardGunsWOMags = ComboBoxFactory{
+		name="ComboBoxRewardGunsWOMags",
+		label="reward_guns_wo_mags_label",
+		items={ "0.25xp", "0.5xp", "0.75xp", "1.0xp", "1.5xp", "2.0xp", "3.0xp", "5.0xp", "10.0xp" },
+		defaultItem=4
+		description="reward_guns_wo_mags_description"
+	}
+	modOptions.ComboBoxRewardGunsWOMags = ComboBoxRewardGunsWOMags
 
-	
+	local ComboBoxChanceGunsWOMags = ComboBoxFactory{
+		name="ComboBoxChanceGunsWOMags",
+		label="chance_guns_wo_mags_label",
+		items={ "1/1", "1/2", "1/3", "1/4", "1/5", "1/6", "1/7", "1/8", "1/9", "1/10" },
+		defaultItem=1
+		description="chance_guns_wo_mags_description"
+	}
+	modOptions.ComboBoxChanceGunsWOMags = ComboBoxChanceGunsWOMags
+
+	local ComboBoxReductionGunsWOMags = ComboBoxFactory{
+		name="ComboBoxReductionGunsWOMags",
+		label="reduction_guns_wo_mags_label",
+		items={ 
+			getText("UI_options_STRELOAD_B42_reduction_guns_wo_mags_item_yes"),
+			getText("UI_options_STRELOAD_B42_reduction_guns_wo_mags_item_no") 
+		},
+		defaultItem=1
+		description="reduction_guns_wo_mags_description"
+	}
 end
 
-
---Default options.
-local SETTINGS = { 
-	options = {
-		dropdown4 = 4,
-		dropdown5 = 1,
-		dropdown6 = 2,
-	},
-	names= {
-		dropdown4 = "Xp reward (Guns without clips/mags)",
-		dropdown5 = "Xp chance (Guns without clips/mags)",
-		dropdown6 = "XP Reduction at lvl 5+ (Guns without clips/mags)",
-	},
-}
-
--- Connecting the options to the menu, so user can change them.
-if ModOptions and ModOptions.getInstance then
-	local settings = ModOptions:getInstance(SETTINGS)
-	
-	local drop4 = settings:getData("dropdown4")
-	drop4[1] = "0.25xp(vanilla lvl 5-10)"
-	drop4[2] = "0.5xp"
-	drop4[3] = "0.75xp"
-	drop4[4] = "1.0xp(vanilla lvl 0-4)"
-	drop4[5] = "1.5xp"
-	drop4[6] = "2.0xp"
-	drop4[7] = "3.0xp"
-	drop4[8] = "5.0xp"
-	drop4[9] = "10.0xp"
-	drop4.tooltip = "Xp gained each time you pass the 1 in X chance. (Guns without clips/mags)"
-	local drop5 = settings:getData("dropdown5")
-	drop5[1] = "1(xp every bullet, also is vanilla lvl 0-4)"
-	drop5[2] = "2"
-	drop5[3] = "3(vanilla lvl 5-10)"
-	drop5[4] = "4"
-	drop5[5] = "5"
-	drop5[6] = "6"
-	drop5[7] = "7"
-	drop5[8] = "8"
-	drop5[9] = "9"
-	drop5[10] = "10"
-	drop5.tooltip = "Chance to get xp reward, one in X (1/X), each bullet loaded. Setting 1 means xp always rewarded, 2 means xp rewarded half(1/2) the time, etc. (Guns without clips/mags)"
-	local drop6 = settings:getData("dropdown6")
-	drop6[1] = "Yes. Keep the vanilla XP reduction. (Guns without clips/mags)"
-	drop6[2] = "No. Remove the vanilla XP reduction. (Guns without clips/mags)"
-	drop6.tooltip = "Vanilla game is set to 'Yes'.  This lowers your xp gains from Revolvers, Shotguns, and any other guns that do not use a clip or magazine when you hit Reloading level 5 or beyond."
-	
-end
-
-StandardizedReloadingXP_global = {}
-StandardizedReloadingXP_global.SETTINGS = SETTINGS
+return modOptions
